7286d2bf02f51ad0e885b8782e0cad4c703cd08a
alisher-rizoqulov2 <rizoqulovalisher820@gmail.com>
2025-06-04 08:11:21 +0500

    ozgartirildi

--- src/main.ts
+++ src/main.ts
@@ -35,25 +35,12 @@ async function start() {
       )
       .build();
 
-    app.enableCors({
-      origin: (origin, callback) => {
-        const allowedOrigins = [
-          "http://13.203.128.82:3000",
-          "http://localhost:3000",
-          "https://Fermer.uz",
-          "https://admin.Fermer.uz",
-          "https://Fermer-admin.vercel.app",
-          "https://Fermer-ishchi.vercel.app",
-        ];
-        if (!origin || allowedOrigins.includes(origin)) {
-          callback(null, true);
-        } else {
-          callback(new BadRequestException("Not allowed by CORS"));
-        }
-      },
-      methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-      credentials: true, //cookie va header,
-    });
+      app.enableCors({
+        origin: true,
+        methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+        credentials: true,
+      });
+      
 
     const document = SwaggerModule.createDocument(app, config);
     SwaggerModule.setup("api/docs", app, document);
